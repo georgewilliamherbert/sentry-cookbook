56c892a8081a7024f43a0acf8962f9d5bcdff40c
Milos Gajdos <milosgajdos83@gmail.com>
2012-06-22 13:33:01 +0100

    Updated cookbook metadata as python-stuff cookbook might depend on python cookbook lets see

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,4 @@ license          "BSD"
 description      "Installs/Configures django sentry envent management tool"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.2"
+depends		 "python"
