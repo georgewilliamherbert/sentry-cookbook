984d4d7072a20874f4ca57924c0f6325983a3213
Milos Gajdos <milosgajdos83@gmail.com>
2012-06-22 11:20:15 +0100

    Just adjusting a version of the cookbok

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "milosgajdos83@gmail.com"
 license          "BSD"
 description      "Installs/Configures django sentry envent management tool"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
