2fbf41cae392b9a7ebc683ebecefe59937bb590b
Milos Gajdos <milosgajdos83@gmail.com>
2012-06-22 15:25:17 +0100

    I forgot to increase a version number of this cookbook after I've added new provider

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "milosgajdos83@gmail.com"
 license          "BSD"
 description      "Installs/Configures django sentry envent management tool"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
 depends		 "python"
